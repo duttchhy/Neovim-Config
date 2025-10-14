5c481bda31415a91faa936f849478a6b59869e5e
Bram <frainbreez3@gmail.com>
2025-10-14 22:37:32 +1300

    Kitty compatibility added

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,30 +1,178 @@
+-- lua/plugins/telescope.lua
 return {
+  -- Main Telescope plugin
   {
     "nvim-telescope/telescope.nvim",
     cmd = "Telescope",
-    tag = '0.1.8',
+    version = false, -- use HEAD
     dependencies = {
-      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-      { "nvim-lua/plenary.nvim"},
+      "nvim-lua/plenary.nvim",
+      -- fzf-native for fast sorting
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+        config = function()
+          local ok, _ = pcall(require("telescope").load_extension, "fzf")
+          if not ok then
+            print("Failed to load telescope-fzf-native.nvim")
+          end
+        end,
+      },
+      -- dressing.nvim for vim.ui improvement
+      {
+        "stevearc/dressing.nvim",
+        lazy = true,
+        init = function()
+          vim.ui.select = function(...)
+            require("lazy").load({ plugins = { "dressing.nvim" } })
+            return vim.ui.select(...)
+          end
+          vim.ui.input = function(...)
+            require("lazy").load({ plugins = { "dressing.nvim" } })
+            return vim.ui.input(...)
+          end
+        end,
+      },
     },
     keys = {
-      { "<leader>,", "<cmd>Telescope buffers<cr>", desc = "Switch Buffer" },
-      { "<leader>/", "<cmd>Telescope live_grep<cr>", desc = "Grep" },
+      -- Buffers & Files
+      { "<leader>,", "<cmd>Telescope buffers sort_mru=true sort_lastused=true<cr>", desc = "Switch Buffer" },
+      { "<leader>/", "<cmd>Telescope live_grep<cr>", desc = "Grep (Root Dir)" },
+      { "<leader>:", "<cmd>Telescope command_history<cr>", desc = "Command History" },
+      { "<leader><space>", "<cmd>Telescope find_files<cr>", desc = "Find Files (Root Dir)" },
+      { "<leader>fb", "<cmd>Telescope buffers sort_mru=true sort_lastused=true ignore_current_buffer=true<cr>", desc = "Buffers" },
+      { "<leader>fc", "<cmd>Telescope find_files cwd=~/.config/nvim<cr>", desc = "Find Config File" },
+      { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files (Root Dir)" },
+      { "<leader>fF", "<cmd>Telescope find_files cwd_only=true<cr>", desc = "Find Files (cwd)" },
+      { "<leader>fg", "<cmd>Telescope git_files<cr>", desc = "Find Files (git-files)" },
+      { "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "Recent" },
+      -- Git
+      { "<leader>gc", "<cmd>Telescope git_commits<CR>", desc = "Commits" },
+      { "<leader>gs", "<cmd>Telescope git_status<CR>", desc = "Status" },
+      -- Search & Registers
+      { '<leader>s"', "<cmd>Telescope registers<cr>", desc = "Registers" },
+      { "<leader>sa", "<cmd>Telescope autocommands<cr>", desc = "Auto Commands" },
+      { "<leader>sb", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Buffer" },
+      { "<leader>sc", "<cmd>Telescope command_history<cr>", desc = "Command History" },
+      { "<leader>sC", "<cmd>Telescope commands<cr>", desc = "Commands" },
+      { "<leader>sd", "<cmd>Telescope diagnostics bufnr=0<cr>", desc = "Document Diagnostics" },
+      { "<leader>sD", "<cmd>Telescope diagnostics<cr>", desc = "Workspace Diagnostics" },
+      { "<leader>sh", "<cmd>Telescope help_tags<cr>", desc = "Help Pages" },
+      { "<leader>sH", "<cmd>Telescope highlights<cr>", desc = "Search Highlight Groups" },
+      { "<leader>sj", "<cmd>Telescope jumplist<cr>", desc = "Jumplist" },
+      { "<leader>sk", "<cmd>Telescope keymaps<cr>", desc = "Key Maps" },
+      { "<leader>sl", "<cmd>Telescope loclist<cr>", desc = "Location List" },
+      { "<leader>sM", "<cmd>Telescope man_pages<cr>", desc = "Man Pages" },
+      { "<leader>sm", "<cmd>Telescope marks<cr>", desc = "Jump to Mark" },
+      { "<leader>so", "<cmd>Telescope vim_options<cr>", desc = "Options" },
+      { "<leader>sR", "<cmd>Telescope resume<cr>", desc = "Resume" },
+      { "<leader>sq", "<cmd>Telescope quickfix<cr>", desc = "Quickfix List" },
     },
     opts = function()
+      local telescope = require("telescope")
       local actions = require("telescope.actions")
-      return {
+      local action_state = require("telescope.actions.state")
+
+      -- Trouble integration
+      local open_with_trouble = function(...)
+        return require("trouble.providers.telescope").open_with_trouble(...)
+      end
+
+      local function find_command()
+        if vim.fn.executable("rg") == 1 then
+          return { "rg", "--files", "--color", "never", "-g", "!.git" }
+        elseif vim.fn.executable("fd") == 1 then
+          return { "fd", "--type", "f", "--color", "never", "-E", ".git" }
+        elseif vim.fn.executable("fdfind") == 1 then
+          return { "fdfind", "--type", "f", "--color", "never", "-E", ".git" }
+        elseif vim.fn.executable("find") == 1 and vim.fn.has("win32") == 0 then
+          return { "find", ".", "-type", "f" }
+        elseif vim.fn.executable("where") == 1 then
+          return { "where", "/r", ".", "*" }
+        end
+      end
+
+      telescope.setup({
         defaults = {
           prompt_prefix = " ",
           selection_caret = " ",
+          get_selection_window = function()
+            local wins = vim.api.nvim_list_wins()
+            table.insert(wins, 1, vim.api.nvim_get_current_win())
+            for _, win in ipairs(wins) do
+              local buf = vim.api.nvim_win_get_buf(win)
+              if vim.bo[buf].buftype == "" then
+                return win
+              end
+            end
+            return 0
+          end,
           mappings = {
-            i = { ["<C-f>"] = actions.preview_scrolling_down, ["<C-b>"] = actions.preview_scrolling_up },
-            n = { ["q"] = actions.close },
+            i = {
+              ["<c-t>"] = open_with_trouble,
+              ["<a-t>"] = open_with_trouble,
+              ["<C-Down>"] = actions.cycle_history_next,
+              ["<C-Up>"] = actions.cycle_history_prev,
+              ["<C-f>"] = actions.preview_scrolling_down,
+              ["<C-b>"] = actions.preview_scrolling_up,
+            },
+            n = {
+              ["q"] = actions.close,
+            },
           },
         },
+        pickers = {
+          find_files = {
+            find_command = find_command,
+            hidden = true,
+          },
+        },
+        extensions = {
+          fzf = { fuzzy = true, override_generic_sorter = true, override_file_sorter = true, case_mode = "smart_case" },
+        },
+      })
+
+      -- Load fzf extension
+      pcall(telescope.load_extension, "fzf")
+
+      -- Flash integration if installed
+      if pcall(require, "flash") then
+        local flash = function(prompt_bufnr)
+          require("flash").jump({
+            pattern = "^",
+            label = { after = { 0, 0 } },
+            search = { mode = "search", exclude = function(win)
+              return vim.bo[vim.api.nvim_win_get_buf(win)].filetype ~= "TelescopeResults"
+            end },
+            action = function(match)
+              local picker = require("telescope.actions.state").get_current_picker(prompt_bufnr)
+              picker:set_selection(match.pos[1] - 1)
+            end,
+          })
+        end
+        telescope.setup({
+          defaults = {
+            mappings = { n = { s = flash }, i = { ["<c-s>"] = flash } },
+          },
+        })
+      end
+    end,
+  },
+
+  -- LSP Telescope keymaps
+  {
+    "neovim/nvim-lspconfig",
+    opts = function()
+      local keys = {
+        { "gd", function() require("telescope.builtin").lsp_definitions({ reuse_win = true }) end, desc = "Goto Definition" },
+        { "gr", "<cmd>Telescope lsp_references<cr>", desc = "References" },
+        { "gI", function() require("telescope.builtin").lsp_implementations({ reuse_win = true }) end, desc = "Goto Implementation" },
+        { "gy", function() require("telescope.builtin").lsp_type_definitions({ reuse_win = true }) end, desc = "Goto Type Definition" },
       }
+      for _, key in ipairs(keys) do
+        vim.keymap.set("n", key[1], key[2], { desc = key.desc, silent = true })
+      end
     end,
   },
 }
-
 
