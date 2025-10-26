166b6a135a8ece605cab72418a5ecb56e580587d
Bram <frainbreez3@gmail.com>
2025-10-27 12:32:32 +1300

    CMP fixes

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,119 +1,101 @@
 return {
-  -- Core nvim-cmp engine
-  {
-    "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp", -- LSP source
-      "L3MON4D3/LuaSnip", -- Snippet engine
-      "saadparwaiz1/cmp_luasnip", -- LuaSnip integration
-      "hrsh7th/cmp-buffer", -- Buffer completions
-      "hrsh7th/cmp-path", -- Path completions
-      "hrsh7th/cmp-cmdline", -- Cmdline completions
-      "folke/lazydev.nvim", -- lazydev completion source
+    -- Core nvim-cmp engine
+    {
+        "hrsh7th/nvim-cmp",
+        event = "InsertEnter",
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+            "L3MON4D3/LuaSnip",
+            "saadparwaiz1/cmp_luasnip",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-cmdline",
+            "folke/lazydev.nvim",
+        },
+        config = function()
+            local cmp = require("cmp")
+            local luasnip = require("luasnip")
+
+            -- Load friendly-snippets
+            require("luasnip.loaders.from_vscode").lazy_load()
+
+            -- Lazydev setup
+            require("lazydev").setup({
+                library = {
+                    { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+                },
+            })
+
+            -- 🔧 Patch cmp-path to expand tilde (~)
+            local cmp_path = require("cmp_path")
+            local old_get_cwd = cmp_path.get_cwd
+            cmp_path.get_cwd = function()
+                local cwd = old_get_cwd()
+                if cwd == "" then
+                    cwd = vim.fn.expand("~")
+                end
+                return cwd
+            end
+
+            -- nvim-cmp setup
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        luasnip.lsp_expand(args.body)
+                    end,
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ["<Tab>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_next_item()
+                        elseif luasnip.expand_or_jumpable() then
+                            luasnip.expand_or_jump()
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }),
+                    ["<S-Tab>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_prev_item()
+                        elseif luasnip.jumpable(-1) then
+                            luasnip.jump(-1)
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }),
+                    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+                }),
+                sources = cmp.config.sources({
+                    { name = "lazydev", group_index = 0 },
+                    { name = "nvim_lsp" },
+                    { name = "path" },
+                    { name = "luasnip" },
+                }, {
+                    { name = "buffer" },
+                }),
+                completion = { completeopt = "menu,menuone,noselect" },
+                formatting = {
+                    format = function(entry, vim_item)
+                        if entry.source.name == "path" then
+                            vim_item.kind = " [Path]"
+                        end
+                        return vim_item
+                    end,
+                },
+            })
+
+            -- Cmdline completions
+            cmp.setup.cmdline("/", {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = { { name = "buffer" } },
+            })
+            cmp.setup.cmdline(":", {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = cmp.config.sources(
+                    { { name = "path" } },
+                    { { name = "cmdline" } }
+                ),
+            })
+        end,
     },
-    config = function()
-      local cmp = require("cmp")
-      local luasnip = require("luasnip")
-
-      -- Load friendly-snippets
-      require("luasnip.loaders.from_vscode").lazy_load()
-
-      -- Setup lazydev for global cmp completions
-      require("lazydev").setup({
-        library = {
-          { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-        },
-      })
-
-      -- Add lazydev as a global cmp source
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump()
-            else
-              -- Always fallback to default <Tab> (indentation, etc.)
-              fallback()
-            end
-          end, { "i", "s" }),
-
-          ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-              luasnip.jump(-1)
-            else
-              -- Always fallback to default <S-Tab> (outdent)
-              fallback()
-            end
-          end, { "i", "s" }),
-
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
-        }),
-        sources = cmp.config.sources({
-          { name = "lazydev", group_index = 0 }, -- global lazydev source
-          { name = "nvim_lsp" },
-          { name = "luasnip" },
-        }, {
-          { name = "buffer" },
-        }),
-        completion = {
-          completeopt = "menu,menuone,noselect",
-        },
-      })
-
-      -- Cmdline completion (search)
-      cmp.setup.cmdline("/", {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = { { name = "buffer" } },
-      })
-
-      -- Cmdline completion (commands)
-      cmp.setup.cmdline(":", {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = cmp.config.sources({ { name = "path" } }, { { name = "cmdline" } }),
-      })
-    end,
-  },
-
-  -- blink.cmp for automatic popup & signature help
-  {
-    "saghen/blink.cmp",
-    dependencies = { "rafamadriz/friendly-snippets", "folke/lazydev.nvim" },
-    version = "v0.*",
-    ft = "lua", -- only load blink/lazydev on lua files
-    config = function()
-      require("blink.cmp").setup({
-        keymap = { preset = "default" },
-        appearance = {
-          use_nvim_cmp_as_default = true,
-          nerd_font_variant = "mono",
-        },
-        signature = { enabled = true },
-        sources = {
-          default = { "lazydev", "lsp", "path", "snippets", "buffer", "cmdline" },
-          providers = {
-            lazydev = {
-              name = "LazyDev",
-              module = "lazydev.integrations.blink",
-              score_offset = 100, -- boost lazydev completions
-            },
-            cmdline = {
-              name = "Cmdline",
-              module = "blink.cmp.sources.cmdline",
-              score_offset = 50, -- slightly boost cmdline completions
-            },
-          },
-        },
-      })
-    end,
-  },
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,126 +1,110 @@
 return {
-  {
-    "folke/lazydev.nvim",
-    ft = "lua",
-    opts = {
-      library = {
-        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-      },
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    event = "BufReadPre",
-    dependencies = {
-      { "williamboman/mason.nvim", opts = {} },
-      "williamboman/mason-lspconfig.nvim",
-      "WhoIsSethDaniel/mason-tool-installer.nvim",
-      { "j-hui/fidget.nvim", opts = {} },
-      "hrsh7th/cmp-nvim-lsp",
-    },
-    config = function()
-      -- LspAttach autocmd
-      vim.api.nvim_create_autocmd("LspAttach", {
-        group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
-        callback = function(event)
-          local map = function(keys, func, desc, mode)
-            mode = mode or "n"
-            vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
-          end
+    {
+        "neovim/nvim-lspconfig",
+        event = "BufReadPre",
+        dependencies = {
+            { "williamboman/mason.nvim", opts = {} },
+            "williamboman/mason-lspconfig.nvim",
+            "WhoIsSethDaniel/mason-tool-installer.nvim",
+            { "j-hui/fidget.nvim",       opts = {} },
+            "hrsh7th/cmp-nvim-lsp",
+        },
+        config = function()
+            -- LspAttach autocmd
+            vim.api.nvim_create_autocmd("LspAttach", {
+                group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
+                callback = function(event)
+                    local map = function(keys, func, desc, mode)
+                        mode = mode or "n"
+                        vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+                    end
 
-          -- Telescope-based LSP navigation
-          map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
-          map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-          map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
-          map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
-          map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
-          map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+                    -- Telescope-based LSP navigation
+                    map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+                    map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+                    map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+                    map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
+                    map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+                    map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
 
-          -- Core LSP actions
-          map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-          map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-          vim.keymap.set({ "n", "x" }, "<leader>ca", vim.lsp.buf.code_action, {
-            buffer = event.buf,
-            desc = "LSP: Code Action",
-          })
+                    -- Core LSP actions
+                    map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+                    map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+                    vim.keymap.set({ "n", "x" }, "<leader>ca", vim.lsp.buf.code_action, {
+                        buffer = event.buf,
+                        desc = "LSP: Code Action",
+                    })
+                end,
+            })
+
+            -- Capabilities
+            local capabilities = vim.lsp.protocol.make_client_capabilities()
+            capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+
+            -- LSP servers
+            local servers = {
+                lua_ls = {
+                    settings = {
+                        Lua = {
+                            completion = { callSnippet = "Replace" },
+                            diagnostics = {
+                                globals = { "vim" }, -- helps avoid false 'undefined' errors
+                            },
+                        },
+                    },
+                },
+            }
+
+            local ensure_installed = vim.tbl_keys(servers)
+            require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
+
+            require("mason-lspconfig").setup({
+                handlers = {
+                    function(server_name)
+                        local server = servers[server_name] or {}
+                        server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
+                        require("lspconfig")[server_name].setup(server)
+                    end,
+                },
+            })
+
+            -- Diagnostics configuration
+            vim.diagnostic.config({
+                virtual_text = {
+                    prefix = "●",
+                    spacing = 4,
+                },
+                float = {
+                    border = "rounded",
+                    source = "always",
+                },
+                signs = true,
+                severity_sort = true,
+                update_in_insert = false,
+            })
+
+            -- Define diagnostic signs (works in Neovim 0.10 and 0.11+)
+            local signs = {
+                { name = "DiagnosticSignError", text = " " },
+                { name = "DiagnosticSignWarn", text = " " },
+                { name = "DiagnosticSignHint", text = " " },
+                { name = "DiagnosticSignInfo", text = " " },
+            }
+
+            for _, sign in ipairs(signs) do
+                -- Use new API if available, otherwise fallback
+                if vim.diagnostic.define_sign then
+                    vim.diagnostic.define_sign(sign.name, { text = sign.text, texthl = sign.name })
+                else
+                    vim.fn.sign_define(sign.name, { text = sign.text, texthl = sign.name, numhl = "" })
+                end
+            end
+            --      -- Show diagnostics in a floating window on hover
+            --      vim.api.nvim_create_autocmd("CursorHold", {
+            --        callback = function()
+            --          vim.diagnostic.open_float(nil, { focus = false })
+            --        end,
+            --      })
         end,
-      })
-
-      -- Capabilities
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
-
-      -- LSP servers
-      local servers = {
-        lua_ls = {
-          settings = {
-            Lua = {
-              completion = { callSnippet = "Replace" },
-              diagnostics = {
-                globals = { "vim" }, -- helps avoid false 'undefined' errors
-              },
-            },
-          },
-        },
-        r_language_server = { -- R LSP
-          cmd = { "R", "--slave", "-e", "languageserver::run()" },
-          filetypes = { "r", "rmd" }, -- R and R Markdown files
-          root_dir = function(fname)
-            return vim.fn.getcwd() -- or use a more complex project root detection if desired
-          end,
-        },
-      }
-
-      local ensure_installed = vim.tbl_keys(servers)
-      require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
-
-      require("mason-lspconfig").setup({
-        handlers = {
-          function(server_name)
-            local server = servers[server_name] or {}
-            server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
-            require("lspconfig")[server_name].setup(server)
-          end,
-        },
-      })
-
-      -- Diagnostics configuration
-      vim.diagnostic.config({
-        virtual_text = {
-          prefix = "●",
-          spacing = 4,
-        },
-        float = {
-          border = "rounded",
-          source = "always",
-        },
-        signs = true,
-        severity_sort = true,
-        update_in_insert = false,
-      })
-
-      -- Define diagnostic signs (works in Neovim 0.10 and 0.11+)
-      local signs = {
-        { name = "DiagnosticSignError", text = " " },
-        { name = "DiagnosticSignWarn", text = " " },
-        { name = "DiagnosticSignHint", text = " " },
-        { name = "DiagnosticSignInfo", text = " " },
-      }
-
-      for _, sign in ipairs(signs) do
-        -- Use new API if available, otherwise fallback
-        if vim.diagnostic.define_sign then
-          vim.diagnostic.define_sign(sign.name, { text = sign.text, texthl = sign.name })
-        else
-          vim.fn.sign_define(sign.name, { text = sign.text, texthl = sign.name, numhl = "" })
-        end
-      end
-      --      -- Show diagnostics in a floating window on hover
-      --      vim.api.nvim_create_autocmd("CursorHold", {
-      --        callback = function()
-      --          vim.diagnostic.open_float(nil, { focus = false })
-      --        end,
-      --      })
-    end,
-  },
+    },
 }
