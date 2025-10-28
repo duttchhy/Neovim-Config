c1d76a9ba0a6715ad87fe10ea8f5238a6744167d
Bram <frainbreez3@gmail.com>
2025-10-29 11:27:52 +1300

    Updates to colors, and plugins not used

--- lua/config/colors.lua
+++ lua/config/colors.lua
@@ -1,51 +1,51 @@
---      _____    ____   ____  _________________  _________________      _____    ____   ____  ____   ____  _____      _____ 
+--      _____    ____   ____  _________________  _________________      _____    ____   ____  ____   ____  _____      _____
 --  ___|\    \  |    | |    |/                 \/                 \ ___|\    \  |    | |    ||    | |    ||\    \    /    /|
 -- |    |\    \ |    | |    |\______     ______/\______     ______//    /\    \ |    | |    ||    | |    || \    \  /    / |
 -- |    | |    ||    | |    |   \( /    /  )/      \( /    /  )/  |    |  |    ||    |_|    ||    |_|    ||  \____\/    /  /
--- |    | |    ||    | |    |    ' |   |   '        ' |   |   '   |    |  |____||    .-.    ||    .-.    | \ |    /    /  / 
--- |    | |    ||    | |    |      |   |              |   |       |    |   ____ |    | |    ||    | |    |  \|___/    /  /  
--- |    | |    ||    | |    |     /   //             /   //       |    |  |    ||    | |    ||    | |    |      /    /  /   
--- |____|/____/||\___\_|____|    /___//             /___//        |\ ___\/    /||____| |____||____| |____|     /____/  /    
--- |    /    | || |    |    |   |`   |             |`   |         | |   /____/ ||    | |    ||    | |    |    |`    | /     
--- |____|____|/  \|____|____|   |____|             |____|          \|___|    | /|____| |____||____| |____|    |_____|/      
---   \(    )/       \(   )/       \(                 \(              \( |____|/   \(     )/    \(     )/         )/         
---    '    '         '   '         '                  '               '   )/       '     '      '     '          '          
+-- |    | |    ||    | |    |    ' |   |   '        ' |   |   '   |    |  |____||    .-.    ||    .-.    | \ |    /    /  /
+-- |    | |    ||    | |    |      |   |              |   |       |    |   ____ |    | |    ||    | |    |  \|___/    /  /
+-- |    | |    ||    | |    |     /   //             /   //       |    |  |    ||    | |    ||    | |    |      /    /  /
+-- |____|/____/||\___\_|____|    /___//             /___//        |\ ___\/    /||____| |____||____| |____|     /____/  /
+-- |    /    | || |    |    |   |`   |             |`   |         | |   /____/ ||    | |    ||    | |    |    |`    | /
+-- |____|____|/  \|____|____|   |____|             |____|          \|___|    | /|____| |____||____| |____|    |_____|/
+--   \(    )/       \(   )/       \(                 \(              \( |____|/   \(     )/    \(     )/         )/
+--    '    '         '   '         '                  '               '   )/       '     '      '     '          '
 return function()
-  local status_ok, catppuccin = pcall(require, "catppuccin")
-  if not status_ok then
-    vim.notify("Catppuccin theme not found!", vim.log.levels.WARN)
-    return
-  end
+    local status_ok, catppuccin = pcall(require, "catppuccin")
+    if not status_ok then
+        vim.notify("Catppuccin theme not found!", vim.log.levels.WARN)
+        return
+    end
 
-  catppuccin.setup({
-    flavour = "macchiato",
-    background = { light = "latte", dark = "macchiato" },
-    transparent_background = true,
-    show_end_of_buffer = false,
-    term_colors = true,
-    styles = {
-      comments = { "italic" },
-      conditionals = { "italic" },
-    },
-    integrations = {
-      cmp = true,
-      gitsigns = true,
-      nvimtree = true,
-      telescope = true,
-      treesitter = true,
-      which_key = true,
-      lsp_trouble = true,
-      mason = true,
-      noice = true,
-      fidget = true,
-    },
-  })
+    catppuccin.setup({
+        flavour = "frappe",
+        background = { light = "latte", dark = "macchiato" },
+        transparent_background = true,
+        show_end_of_buffer = false,
+        term_colors = true,
+        styles = {
+            comments = { "italic" },
+            conditionals = { "italic" },
+        },
+        integrations = {
+            cmp = true,
+            gitsigns = true,
+            nvimtree = true,
+            telescope = true,
+            treesitter = true,
+            which_key = true,
+            lsp_trouble = true,
+            mason = true,
+            noice = true,
+            fidget = true,
+        },
+    })
 
-  vim.cmd.colorscheme("catppuccin")
+    vim.cmd.colorscheme("catppuccin")
 
-  -- Apply transparent highlights safely
-  local ok, _ = pcall(function()
-    vim.cmd([[
+    -- Apply transparent highlights safely
+    local ok, _ = pcall(function()
+        vim.cmd([[
       hi Normal guibg=NONE ctermbg=NONE
       hi NormalNC guibg=NONE ctermbg=NONE
       hi SignColumn guibg=NONE ctermbg=NONE
@@ -54,5 +54,5 @@ return function()
       hi LineNr guibg=NONE
       hi NonText guibg=NONE
     ]])
-  end)
+    end)
 end
--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,9 +1,43 @@
 return {
-  {
-    "MeanderingProgrammer/render-markdown.nvim",
-    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
-    ---@module 'render-markdown'
-    ---@type render.md.UserConfig
-    opts = {},
-  },
+    "OXY2DEV/markview.nvim",
+    ft = { "markdown", "Avante", "codecompanion", "opencode_output" },
+    init = function()
+        vim.keymap.set("n", "<C-s>M", function()
+            if vim.bo.filetype == "markdown" then
+                vim.cmd "Markview Toggle"
+            else
+                vim.notify("Only available in markdown", vim.log.levels.WARN, { title = "Markview" })
+            end
+        end, { desc = "Toggle Markview", silent = true })
+    end,
+    opts = {
+        preview = {
+            filetypes = { "markdown", "Avante", "codecompanion", "opencode_output" },
+            ignore_buftypes = {},
+        },
+        max_length = 99999,
+        experimental = { check_rtp_message = false },
+    },
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+        "nvim-tree/nvim-web-devicons",
+    },
+    {
+        "iamcco/markdown-preview.nvim",
+        enabled = false,
+        init = function()
+            vim.keymap.set("n", "<C-s>M", function()
+                if vim.bo.filetype == "markdown" then
+                    vim.cmd "MarkdownPreviewToggle"
+                else
+                    vim.notify("Only available in markdown", vim.log.levels.WARN, { title = "Markdown-Preview" })
+                end
+            end, { desc = "Markdown Preview", silent = true })
+        end,
+        build = function()
+            require("lazy").load { plugins = { "markdown-preview.nvim" } }
+            vim.fn["mkdp#util#install"]()
+        end,
+        ft = "markdown",
+    },
 }
