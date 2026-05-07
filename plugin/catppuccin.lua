80fad8c8d589531d51668868ed8ea466b51c18a5
Bram <bram@duttchhy.com>
2026-05-07 20:20:36 +1200

    I am funny

--- plugin/catppuccin.lua
+++ plugin/catppuccin.lua
@@ -1,61 +1,61 @@
 -- Colorscheme: loaded first to avoid flash of default colors.
-vim.pack.add {
-  'https://github.com/catppuccin/nvim',
-}
+vim.pack.add({
+	"https://github.com/catppuccin/nvim",
+})
 
-require('catppuccin').setup {
-  flavour = 'mocha',
-  background = {
-    light = 'latte',
-    dark = 'mocha',
-  },
-  transparent_background = false,
-  show_end_of_buffer = true,
-  term_colors = true,
-  styles = {
-    comments = { 'italic' },
-    conditionals = { 'italic' },
-    loops = {},
-    functions = {},
-    keywords = {},
-    strings = {},
-    variables = {},
-    numbers = {},
-    booleans = {},
-    properties = {},
-    types = {},
-    operators = {},
-  },
-  integrations = {
-    treesitter = true,
-    native_lsp = {
-      enabled = true,
-      virtual_text = {
-        errors = { 'italic' },
-        hints = { 'italic' },
-        warnings = { 'italic' },
-        information = { 'italic' },
-      },
-      underlines = {
-        errors = { 'underline' },
-        hints = { 'underline' },
-        warnings = { 'underline' },
-        information = { 'underline' },
-      },
-    },
-    blink_cmp = true,
-    aerial = true,
-    bufferline = true,
-    dashboard = true,
-    gitsigns = true,
-    indent_blankline = { enabled = true },
-    markdown = true,
-    mini = { enabled = true },
-    snacks = true,
-    telescope = { enabled = false },
-    trouble = true,
-    which_key = true,
-  },
-}
+require("catppuccin").setup({
+	flavour = "mocha",
+	background = {
+		light = "latte",
+		dark = "mocha",
+	},
+	transparent_background = false,
+	show_end_of_buffer = true,
+	term_colors = true,
+	styles = {
+		comments = { "italic" },
+		conditionals = { "italic" },
+		loops = {},
+		functions = {},
+		keywords = {},
+		strings = {},
+		variables = {},
+		numbers = {},
+		booleans = {},
+		properties = {},
+		types = {},
+		operators = {},
+	},
+	integrations = {
+		treesitter = true,
+		native_lsp = {
+			enabled = true,
+			virtual_text = {
+				errors = { "italic" },
+				hints = { "italic" },
+				warnings = { "italic" },
+				information = { "italic" },
+			},
+			underlines = {
+				errors = { "underline" },
+				hints = { "underline" },
+				warnings = { "underline" },
+				information = { "underline" },
+			},
+		},
+		blink_cmp = true,
+		aerial = true,
+		bufferline = true,
+		dashboard = true,
+		gitsigns = true,
+		indent_blankline = { enabled = true },
+		markdown = true,
+		mini = { enabled = true },
+		snacks = true,
+		telescope = { enabled = false },
+		trouble = true,
+		which_key = true,
+	},
+})
 
-vim.cmd.colorscheme 'catppuccin'
+vim.cmd.colorscheme("catppuccin")
